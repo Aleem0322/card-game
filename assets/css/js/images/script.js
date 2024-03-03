29475a7f37ee3708d396da40978b189fa8f55f60
Aleem0322 <aleemrehman786@hotmail.co.uk>
2024-03-03 11:53:04 +0000

    code back

--- assets/css/js/images/script.js
+++ assets/css/js/images/script.js
@@ -1,149 +1,142 @@
+var dealertotal = 0;
+var playersum = 0;
 
-const VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-const SUITS = ['♠', '♥', '♣', '♦']
+var dealerAceCount = 0;
+var playerAceCount = 0;
 
-const CARD_MODEL = document.createElement('div');
-CARD_MODEL.classList.add('card');
+var hidden;
+var dec; 
 
-const DEALER = document.getElementById("dealer");
-const PLAYER = document.getElementById("player");
-const HIT_BUTTON = document.getElementById("hit-button");
-const PASS_BUTTON = document.getElementById("pass-button");
-const BUTTON_CONTAINER = document.getElementById("button-container");
-const NOTICE = document.getElementById("notice");
-const NEX_HAND_BUTTON = document.getElementById("next-hand-button");
+var canHit = true;
 
-let allDecks = [];
-let dealerHand = [];
-let playerHand = [];
-
-//PART 1
-
-const createDeck = () => {
-    const deck = [];
-    SUITS.forEach((suit) => {
-        VALUES.forEach((value) => {
-            const card = value + suit;
-            deck.push(card)
-        })
-    })
-    return deck;
+window.onload = function() {
+    buildDeck();
+    shuffleDeck();
+    startGame();
 }
 
-const shuffleDecks = (num) => {
-    for (let i = 0; i < num; i++) {
-        const newDeck = createDeck();
-        allDecks = [...allDecks, ...newDeck];
-    }
-}
+function buildDeck() {
+    let values =["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "k"];
+    let types = ["C", "D", "H", "S"];
+    deck = [];
 
-const selectRandomCard = () => {
-    const randomIndex = Math.floor(Math.random() * allDecks.length);
-    const card = allDecks[randomIndex];
-    allDecks.splice(randomIndex, 1)
-    return card;
-}
-
-// PART 2
-
-
-const dealHands = () => {
-    dealerHand = [selectRandomCard(), selectRandomCard()];
-    dealerHand.forEach((card, index) => {
-        const newCard = CARD_MODEL.cloneNode(true);
-        index === 0 ? newCard.classList.add('back') : newCard.innerHTML = card;
-        (card[card.length - 1] === '♦' || card[card.length - 1] === '♥') && newCard.setAttribute('data-red', true);
-        DEALER.append(newCard);
-    })
-    playerHand = [selectRandomCard(), selectRandomCard()];
-    playerHand.forEach((card) => {
-        const newCard = CARD_MODEL.cloneNode(true);
-        newCard.innerHTML = card;
-        (card[card.length - 1] === '♦' || card[card.length - 1] === '♥') && newCard.setAttribute('data-red', true);
-        PLAYER.append(newCard);
-    })
-}
-
-// PART 3
-
-const calcValue = (hand) => {
-    let value = 0;
-    let hasAce = 0;
-    hand.forEach((card) => {
-        if (card.length === 2) {
-            if (card[0] === 'A') {
-                hasAce += 1
-            } else {
-                (card[0] === 'K' || card[0] === 'Q' || card[0] === 'J') ? value += 10 : value += Number(card[0])
-            }
-        } else {
-            value += 10
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + types[i]);
         }
-    })
-    if (hasAce > 0) {
-        value + 11 > 21 ? value += 1 : value += 11;
-        value += (hasAce-1)*1;
     }
-    return value;
+    // console.log(deck);
 }
 
-const hitPlayer = () => {
-    const newCard = selectRandomCard();
-    playerHand.push(newCard);
-    const newCardNode = CARD_MODEL.cloneNode(true);
-    newCardNode.innerHTML = newCard;
-    PLAYER.append(newCardNode)
-    const handValue = calcValue(playerHand);
-    if (handValue > 21) {
-        console.log("bust")
-        alert("bust");
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+    console.log(deck);
+}
+
+function startGame() {
+    hidden = deck.pop();
+    dealertotal += getValue(hidden);
+    dealerAceCount += checkAce(hidden);
+    // console.log(hidden);
+    // console.log(dealertotal);
+    while (dealertotal < 17) {
+        //<img>
+        let cardImg = document.createElement("img")
+        let card = deck.pop();
+        cardImg.src = "./cards/cards" + card + ".jpg";
+        dealertotal += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
+    }
+    console.log(dealertotal);
+
+    for (let i =0; i < 2; i++) {
+        let cardImg = document.createElement("img")
+        let card = deck.pop();
+        cardImg.src = "./cards/cards" + card + ".jpg";
+        playersum += getValue(card);
+        playerAceCountAceCount += checkAce(card);
+        document.getElementById("player-cards").append(cardImg);  
+    }
+    console.log(playersum)
+    document.getElementById("hit").addEventListener("click", hit);
+    document.getElementById("stay").addEventListener("click", stay);
+}
+
+function hit() {
+    if (!canHit) {
+        return;
+    }
+    let cardImg = document.createElement("img")
+    let card = deck.pop();
+    cardImg.src = "./cards/cards" + card + ".jpg";
+    playersum += getValue(card);
+    playerAceCountAceCount += checkAce(card);
+    document.getElementById("player-cards").append(cardImg);
+
+    if (reduceAce(yourSum, yourAceCount) > 21) {
+        canHit = false;
     }
 }
 
-const decideWinner = async () => {
-    let dealerValue = await calcValue(dealerHand);
-    let playerValue = await calcValue(playerHand);
+function stay() {
+    dealertotal = reduceAce(dealertotal, dealerAceCount);
+    playersum = reduceAce(playersum, playerAceCount);
 
-    alert(`Dealer has ${dealerValue}, you have ${playerValue}`)
-    dealerValue > playerValue ? alert("dealer wins!") : alert("player wins!")
+    canHit = false;
+    document.getElementById("hidden").src = "./cards/cards" + hidden + ".jpg"
+
+    let message = "";
+    if (playersum >21) {
+        message = "You Lose";
+    }
+    else if (dealersum > 21) {
+        message = "You win!!";
+    }
+    else if (playersum == dealertotal) {
+        message = "Draw";
+    }
+    else if (playersum > dealertotal) {
+        message = "You win !";
+    }
+    else if (playersum < dealertotal) {
+        message = "You lose";
+    }
+    document.getElementById("dealer-total").innerText = dealertotal;
+    document.getElementById("player-sum").innerText = playersum;
+    document.getElementById("results").innerText = message;
 }
 
-const hitDealer = async () => {
-    //flip green card
-    const hiddenCard = DEALER.children[0];
-    hiddenCard.classList.remove("back");
-    hiddenCard.innerHTML = dealerHand[0];
-    //clac hand value
-    let handValue = await calcValue(dealerHand);
-    if (handValue < 16) {
-        let newCard = selectRandomCard();
-        dealerHand.push(newCard);
-        const newCardNode = CARD_MODEL.cloneNode(true);
-        newCardNode.innerHTML = newCard;
-        DEALER.append(newCardNode)
-        handValue = await calcValue(dealerHand);
+function getValue(card) {
+    let data = card.split("-");
+    let value = data[0];
+
+    if (isNaN(value)) {
+        if (value == "A") {
+            return 11;
+        }
+        return 10;
     }
 
-    if (handValue < 16) {
-        hitDealer();
-    }
-    else if (handValue === 21) {
-        alert("dealer has 21!")
-    }
-    else if (handValue > 21) {
-        alert("dealer bust")
-    }
-    else {
-        decideWinner();
-    }
+    return parseInt(value);
 }
 
+function checkAce(card) {
+    if (card[0] == "A") {
+        return 1;
+    }
+    return 0;
+}
 
-
-HIT_BUTTON.addEventListener('click', hitPlayer);
-PASS_BUTTON.addEventListener('click', hitDealer)
-
-
-
-shuffleDecks(5);
-dealHands()
+function reduceAce(playersum, playerAceCount) {
+    while (playersum > 21 && playerAceCount > 0) {
+        playersum -= 10;
+        playerAceCount -= 1;
+    }
+    return playersum;
+}
